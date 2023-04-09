e009946abba07ac677f0d25dd83aec0f1da675d8
evkes <evkessler2002@gmail.com>
2023-04-08 20:12:54 -0400

    first commit

---
+++ Driver.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Driver
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\nWelcome to the Adkins house! Entering the study room. To leave the house, please type exit to jump out of the nearest window.\n");
+        /*
+         * Guest Room: A room filled with numerous torture devices. Who said anything about welcome guests? :: Kitchen
+         * Library: Better version of the study. It has all of the different books that one may want. Make sure that you stay quiet or the mean librarian will slap you! :: Holodeck, Trophy Room, Study
+         * Kitchen: This amazing culinary art studio has it all: cheese cellar, wine racks, and a 16 stove burner. With its pizza oven, it makes for the perfect Italian getaway. :: Study, Guest Room
+         * Study: Do you love being disturbed while working? This room has it all. It is the central hub to the whole house. It has a giant wall of computers and amazing lighting, but doors that exit out into numerous different rooms. :: Kitchen, Library, Bedroom
+         * Holodeck: A room that can disguise itself in a variety of ways. Experience a lush, humid rainforest, a speakeasy of the 1920’s, or the dungeons of Cooper Library. :: Library
+         * Trophy Room: Spacious room with oak wood as far as the eye can see, shelves filled to the brim with trophies and obscure collections, it really makes you wonder who they belong to. :: Bedroom, Library
+         * Bedroom: A lavished bed adorns the center of this room, with long curtains, beautiful rugs, and gilded furniture acting as little details to truly make this a great bedroom. :: Study, Trophy Room
+         */
+        
+
+        /*
+         * Create a scanner, put the inital room (entrance) to study
+         * While the user's input is not exit, continue to traverse the map. 
+         * If there is an invalid selection, then print out "Invalid exit."
+         * After you are done selecting, print out "Please choose an exit" and get next exit
+         */
+        
+    }
+
+}
+
---
+++ Room.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Room
+{
+    String name;
+    String description;
+    ArrayList<String> exits;
+    
+    /**
+     * Initialize a room
+     * @param name the name of the room
+     * @param description the description of the room
+     */
+    public Room(String name, String description)
+    {
+        
+    }
+    /* Generate getters and setters for the naems and descriptions */
+    
+    /**
+     * Adds an exit to the room
+     * @param exit room name of the exit to be added to the room
+     */
+    public void addExit(String exit)
+    {
+        
+    }
+
+    /**
+     * List all of the rooms as a string
+     * @return returns all of the names of the rooms on new lines
+     */
+    public String listExits()
+    {
+        
+    }
+
+    /**
+     * Generates a string representation of the room using the name and description and lists all of the exits.
+     */
+    public String toString()
+    {
+        
+    }
+}
